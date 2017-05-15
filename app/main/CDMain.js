4eb4dfbe9339b3924682f274f30062ff80bcf7ab
urnorchrislee <18513468484@163.com>
2017-05-15 15:15:32 +0800

    自定义小气泡

--- app/main/CDMain.js
+++ app/main/CDMain.js
@@ -39,25 +39,37 @@ export default class extends Component {
                          <CDRead />
                      </TabNavigator.Item>
                 */}
-                {this._renderItem("Reading", 'icon_tabbar_homepage', 'icon_tabbar_homepage_selected', 'reading', <CDRead />)}
+                {this._renderItem("Reading", 'icon_tabbar_homepage', 'icon_tabbar_homepage_selected', 'reading', <CDRead />, '10')}
 
                 {/*park*/}
-                {this._renderItem("Parking", 'icon_tabbar_merchant_normal', 'icon_tabbar_merchant_selected', 'parking', <CDPark />)}
+                {this._renderItem("Parking", 'icon_tabbar_merchant_normal', 'icon_tabbar_merchant_selected', 'parking', <CDPark />, '1')}
 
                 {/*discover*/}
-                {this._renderItem("Discovering", 'icon_tabbar_nearby_normal', 'icon_tabbar_nearby_selected', 'discovering', <CDDiscover />)}
+                {this._renderItem("Discovering", 'icon_tabbar_nearby_normal', 'icon_tabbar_nearby_selected', 'discovering', <CDDiscover />, '2')}
 
                 {/*mine*/}
-                {this._renderItem("Mine", 'icon_tabbar_mine', 'icon_tabbar_mine_selected', 'mine', <CDMine />)}
+                {this._renderItem("Mine", 'icon_tabbar_mine', 'icon_tabbar_mine_selected', 'mine', <CDMine />, '')}
 
             </TabNavigator>
         );
     }
 
-    _renderItem(title, renderIcon, renderSelectedIcon, selectedTab, component){
+    /**
+     * 封装tab
+     * @param title 标题
+     * @param renderIcon 渲染图标
+     * @param renderSelectedIcon 选中图标
+     * @param selectedTab 选中tab
+     * @param component 组件
+     * @param badgeText 气泡效果
+     * @returns {XML}
+     * @private
+     */
+    _renderItem(title, renderIcon, renderSelectedIcon, selectedTab, component, badgeText){
         return (
             <TabNavigator.Item
                 selected={this.state.selectedTab === selectedTab}
+                renderBadge = {()=>this._renderBadge(badgeText)}
                 title={title}
                 selectedTitleStyle = {styles.selectedTitleStyle}
                 renderIcon={() => <Image source={{uri:renderIcon}} style={styles.iconStyle}/>}
@@ -65,6 +77,16 @@ export default class extends Component {
                 onPress={() => this.setState({ selectedTab: selectedTab })}>
                 {component}
             </TabNavigator.Item>
+        )
+    }
+
+    _renderBadge(badgeText){
+        if(badgeText == '') return <View />;
+
+        return (
+            <View style={styles.badgeStyle}>
+                <Text style={styles.badgeTextStyle}>{badgeText}</Text>
+            </View>
         )
     }
 }
@@ -76,6 +98,19 @@ const styles = StyleSheet.create({
     },
     selectedTitleStyle: {
         color: 'rgba(62, 184, 175, 1)'
+    },
+    badgeStyle: {
+        width: 18,
+        height: 18,
+        backgroundColor: 'red',
+        borderRadius: 9,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    badgeTextStyle:{
+        fontSize: 12,
+        color: '#fff',
+        backgroundColor:'transparent'
     }
 });
 
