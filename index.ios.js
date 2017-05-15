9c9d4fe5349d86300282129759b0ddcc1bc55153
urnorchrislee <18513468484@163.com>
2017-05-15 16:48:31 +0800

    初始化导航栏配置

--- index.ios.js
+++ index.ios.js
@@ -6,18 +6,32 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+  AppRegistry
 } from 'react-native';
 
 import CDMain from './app/main/CDMain';
 
+import NavigationExperimental from 'react-native-deprecated-custom-components';
+
+
 export default class reading extends Component {
   render() {
     return (
-      <CDMain />
+        <NavigationExperimental.Navigator
+            // 初始化路由
+            initialRoute={{ name: CDMain, component: CDMain }}
+
+            // 路由跳转过渡方式
+            configureScene={(route) => {
+              return NavigationExperimental.Navigator.SceneConfigs.HorizontalSwipeJumpFromRight;
+            }}
+
+            // 实例化成组件
+            renderScene={(route, navigator) => {
+              let Component = route.component;
+              return <Component {...route.props} navigator={navigator} />
+            }}
+        />
     );
   }
 }
